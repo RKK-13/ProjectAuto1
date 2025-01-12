4a3a95e8796068ea0120c10146c5a075c5b8cfc8
RKK-13 <Ranjithkumarkrishnan1@outlook.com>
2025-01-12 11:46:46 -0600

    Add github workflow actions

---
+++ add.py
@@ -0,0 +1,9 @@
+def add_number (a,b,c):
+    return a+b+c
+if __name__=="__main__":
+    num1 = 10
+    num2 = 20
+    num3 = 30
+    result = add_number(num1,num2,num3)
+    print(f"the result of {num1},{num2},{num3} is {result}")
+
